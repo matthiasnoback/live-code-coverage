9127eb3172a4589d0b04d9bc4649b91b96895e1e
Matthias Noback <matthiasnoback@gmail.com>
2018-02-07 12:11:57 +0100

    Add remote bootstrapping relying on global variables

--- src/LiveCodeCoverage/LiveCodeCoverage.php
+++ src/LiveCodeCoverage/LiveCodeCoverage.php
@@ -45,9 +45,39 @@ final class LiveCodeCoverage
         return $liveCodeCoverage;
     }
 
+    public static function bootstrapRemoteCoverage($storageDirectory, $phpunitConfigFilePath = null)
+    {
+        $coverageGroup = isset($_GET['coverage_group']) ? $_GET['coverage_group'] :
+            (isset($_COOKIE['coverage_group']) ? $_COOKIE['coverage_group'] : null);
+
+        $coverageId = isset($_GET['coverage_id']) ? $_GET['coverage_id'] :
+            (isset($_COOKIE['coverage_id']) ? $_COOKIE['coverage_id'] : 'live-coverage');
+
+        $storageDirectory .= ($coverageGroup ? '/' . $coverageGroup : '');
+
+        if (isset($_GET['export_code_coverage'])) {
+            self::exportCoverageData($storageDirectory);
+            exit;
+        }
+
+        return self::bootstrap($storageDirectory, $phpunitConfigFilePath, $coverageId);
+    }
+
     private function start()
     {
         $this->codeCoverage->start($this->coverageId);
+    }
+
+    /**
+     * @param $coverageDirectory
+     * @return void
+     */
+    public static function exportCoverageData($coverageDirectory)
+    {
+        $codeCoverage = Storage::loadFromDirectory($coverageDirectory);
+
+        header('Content-Type: text/plain');
+        echo serialize($codeCoverage);
     }
 
     public function stopAndSaveOnExit()
